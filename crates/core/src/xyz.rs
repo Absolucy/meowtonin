4d36c45e492c2be5669bc8cc4ea35345fcd5c481
Lucy <lucy@absolucy.moe>
2024-04-15 10:08:16 -0400

    Use saturating operations; const impls

--- crates/core/src/xyz.rs
+++ crates/core/src/xyz.rs
@@ -8,40 +8,42 @@ pub struct ByondXYZ(pub CByondXYZ);
 
 impl ByondXYZ {
 	#[inline]
-	pub fn new(x: i16, y: i16, z: i16) -> Self {
+	pub const fn new(x: i16, y: i16, z: i16) -> Self {
 		Self(CByondXYZ { x, y, z, junk: 0 })
 	}
 
 	#[inline(always)]
-	pub fn x(&self) -> i16 {
+	pub const fn x(&self) -> i16 {
 		self.0.x
 	}
 
 	#[inline(always)]
-	pub fn y(&self) -> i16 {
+	pub const fn y(&self) -> i16 {
 		self.0.y
 	}
 
 	#[inline(always)]
-	pub fn z(&self) -> i16 {
+	pub const fn z(&self) -> i16 {
 		self.0.z
 	}
 
-	pub fn block_size(&self, other: &ByondXYZ) -> (u16, u16) {
-		let dx = (self.x() as i32 - other.x() as i32).unsigned_abs() as u16 + 1;
-		let dy = (self.y() as i32 - other.y() as i32).unsigned_abs() as u16 + 1;
+	pub const fn block_size(&self, other: &ByondXYZ) -> (u16, u16) {
+		let (our_x, our_y) = (self.x() as i32, self.y() as i32);
+		let (other_x, other_y) = (other.x() as i32, other.y() as i32);
+		let dx = (our_x.saturating_sub(other_x)).unsigned_abs() as u16 + 1;
+		let dy = (our_y.saturating_sub(other_y)).unsigned_abs() as u16 + 1;
 		(dx, dy)
 	}
 
-	pub fn total_block_size(&self, other: &ByondXYZ) -> u16 {
+	pub const fn total_block_size(&self, other: &ByondXYZ) -> u16 {
 		let (w, h) = self.block_size(other);
 		w.saturating_mul(h)
 	}
 
 	pub fn distance(&self, other: &ByondXYZ) -> f64 {
-		let dx = (self.x() - other.x()) as f64;
-		let dy = (self.y() - other.y()) as f64;
-		let dz = (self.z() - other.z()) as f64;
+		let dx = (self.x().saturating_sub(other.x())) as f64;
+		let dy = (self.y().saturating_sub(other.y())) as f64;
+		let dz = (self.z().saturating_sub(other.z())) as f64;
 
 		(dx * dx + dy * dy + dz * dz).sqrt()
 	}
@@ -105,18 +107,18 @@ impl Add for ByondXYZ {
 
 	fn add(self, other: ByondXYZ) -> ByondXYZ {
 		ByondXYZ::new(
-			self.x() + other.x(),
-			self.y() + other.y(),
-			self.z() + other.z(),
+			self.x().saturating_add(other.x()),
+			self.y().saturating_add(other.y()),
+			self.z().saturating_add(other.z()),
 		)
 	}
 }
 
 impl AddAssign for ByondXYZ {
 	fn add_assign(&mut self, other: ByondXYZ) {
-		self.0.x += other.0.x;
-		self.0.y += other.0.y;
-		self.0.z += other.0.z;
+		self.0.x = self.0.x.saturating_add(other.x());
+		self.0.y = self.0.y.saturating_add(other.y());
+		self.0.z = self.0.z.saturating_add(other.z());
 	}
 }
 
@@ -125,17 +127,17 @@ impl Sub for ByondXYZ {
 
 	fn sub(self, other: ByondXYZ) -> ByondXYZ {
 		ByondXYZ::new(
-			self.x() - other.x(),
-			self.y() - other.y(),
-			self.z() - other.z(),
+			self.x().saturating_sub(other.x()),
+			self.y().saturating_sub(other.y()),
+			self.z().saturating_sub(other.z()),
 		)
 	}
 }
 
 impl SubAssign for ByondXYZ {
 	fn sub_assign(&mut self, other: ByondXYZ) {
-		self.0.x -= other.0.x;
-		self.0.y -= other.0.y;
-		self.0.z -= other.0.z;
+		self.0.x = self.0.x.saturating_sub(other.x());
+		self.0.y = self.0.y.saturating_sub(other.y());
+		self.0.z = self.0.z.saturating_sub(other.z());
 	}
 }
