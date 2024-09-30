bfb516d9c3678c4197f18b4d0392653f0e5101d9
Lucy <lucy@absolucy.moe>
2024-09-30 12:21:47 -0400

    i forgot why that was an option tbh

--- crates/core/src/sync.rs
+++ crates/core/src/sync.rs
@@ -3,23 +3,21 @@ use crate::{byond, sys::CByondValue, ByondValue};
 use std::os::raw::c_void;
 
 struct CallbackData<F: FnOnce() -> ByondValue + Send> {
-	callback: Option<F>,
+	callback: F,
 }
 
 extern "C-unwind" fn trampoline<F: FnOnce() -> ByondValue + Send>(
 	data: *mut c_void,
 ) -> CByondValue {
 	let data = unsafe { Box::from_raw(data as *mut CallbackData<F>) };
-	(data.callback.unwrap())().into_inner()
+	(data.callback)().into_inner()
 }
 
 pub fn thread_sync<F>(callback: F, block: bool) -> ByondValue
 where
 	F: FnOnce() -> ByondValue + Send + 'static,
 {
-	let data = Box::new(CallbackData {
-		callback: Some(callback),
-	});
+	let data = Box::new(CallbackData { callback });
 	let data_ptr = Box::into_raw(data) as *mut c_void;
 
 	unsafe { byond().Byond_ThreadSync(Some(trampoline::<F>), data_ptr, block) }.into()
