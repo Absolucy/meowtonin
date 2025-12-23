55ccfae01a669ee6bcd864d47675cfba9d245d78
Lucy <lucy@absolucy.moe>
2025-12-22 21:13:18 -0500

    woohoo we can get rid of these stupid functions now!

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -91,8 +91,5 @@ pub fn dmb_version() -> sys::u4c {
 pub fn setup_once() {
 	static SETUP: Once = Once::new();
 
-	SETUP.call_once(|| {
-		let _ = sync::is_main_thread(); // initialize main thread OnceCell
-		std::panic::set_hook(Box::new(panic::panic_hook))
-	});
+	SETUP.call_once(|| std::panic::set_hook(Box::new(panic::panic_hook)));
 }
--- crates/core/src/sync.rs
+++ crates/core/src/sync.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: 0BSD
 use crate::{ByondValue, byond, sys::CByondValue};
-use std::{cell::Cell, os::raw::c_void, sync::OnceLock, thread::ThreadId};
+use std::os::raw::c_void;
 
 struct CallbackData<F: FnOnce() -> ByondValue + Send> {
 	callback: F,
@@ -9,7 +9,6 @@ struct CallbackData<F: FnOnce() -> ByondValue + Send> {
 extern "C-unwind" fn trampoline<F: FnOnce() -> ByondValue + Send>(
 	data: *mut c_void,
 ) -> CByondValue {
-	let _guard = ThreadSyncGuard::new();
 	let data = unsafe { Box::from_raw(data as *mut CallbackData<F>) };
 	unsafe { (data.callback)().0 }
 }
@@ -22,56 +21,4 @@ where
 	let data_ptr = Box::into_raw(data) as *mut c_void;
 
 	ByondValue(unsafe { byond().Byond_ThreadSync(Some(trampoline::<F>), data_ptr, block) })
-}
-
-thread_local! {
-	static THREAD_SYNC_DEPTH: Cell<usize> = const { Cell::new(0) };
-}
-
-/// Checks to see if we're in a ThreadSync call or not.
-pub fn is_in_thread_sync() -> bool {
-	THREAD_SYNC_DEPTH.with(|depth| depth.get() > 0)
-}
-
-/// Simple RAII counter to mark if we're in a ThreadSync call or not, due to
-/// differing refcounting behavior.
-///
-/// This uses a counter instead of a bool, to account for weird cases involving
-/// potentially nested ThreadSync calls.
-///
-/// I hate that this is needed.
-struct ThreadSyncGuard;
-
-impl ThreadSyncGuard {
-	fn new() -> Self {
-		THREAD_SYNC_DEPTH.with(|depth| depth.set(depth.get() + 1));
-		Self
-	}
-}
-
-impl Drop for ThreadSyncGuard {
-	fn drop(&mut self) {
-		THREAD_SYNC_DEPTH.with(|depth| {
-			let current = depth.get();
-			assert!(
-				current > 0,
-				"ThreadSyncGuard somehow dropped more than created, WTF DID YOU DO???"
-			);
-			depth.set(current - 1);
-		});
-	}
-}
-
-/// Checks to see if we're on the main thread or not.
-pub fn is_main_thread() -> bool {
-	static MAIN_THREAD_ID: OnceLock<ThreadId> = OnceLock::new();
-
-	let thread_id = std::thread::current().id();
-	*MAIN_THREAD_ID.get_or_init(|| thread_id) == thread_id
-}
-
-/// Returns if we should manually increment a persistent ref.
-/// This returns true if we're on the main thread, and NOT in threadsync.
-pub fn should_setup_ref_counting() -> bool {
-	is_main_thread() && !is_in_thread_sync()
 }
